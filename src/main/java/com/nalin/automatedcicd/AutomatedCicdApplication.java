25c04ed537a786ea1d0e02fc148cf27d661a9316
Nalin Chaudhary <nalin.chaudhary@qvantel.com>
2021-04-28 21:52:49 +0300

    Change to see CICD working

--- src/main/java/com/nalin/automatedcicd/AutomatedCicdApplication.java
+++ src/main/java/com/nalin/automatedcicd/AutomatedCicdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutomatedCicdApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(AutomatedCicdApplication.class, args);
     }
 
