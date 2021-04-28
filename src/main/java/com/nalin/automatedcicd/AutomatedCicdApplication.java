81b71ce06b5f881b1c0983f93261433a4dce0a2c
Nalin Chaudhary <nalin.chaudhary@qvantel.com>
2021-04-28 17:00:18 +0300

    first commit

---
+++ src/main/java/com/nalin/automatedcicd/AutomatedCicdApplication.java
@@ -0,0 +1,13 @@
+package com.nalin.automatedcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nalin/automatedcicd/AutomatedCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalin.automatedcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
